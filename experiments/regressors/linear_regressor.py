70b14dff9a588b5a26f6f95c0568193830854df8
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-12 08:06:05 +0100

    updating linear regressor

--- experiments/regressors/linear_regressor.py
+++ experiments/regressors/linear_regressor.py
@@ -8,16 +8,19 @@ reload(experiments.transformers.pca_transformer)
 
 class LinearRegressor(BaseEstimator, RegressorMixin):
         
-        def __init__(self, n_components=2):
-            self.n_components = n_components
-            self.transformer = experiments.transformers.pca_transformer.PCATransformer(n_components=self.n_components)
+        def __init__(self, n_pca_components=0):
+            self.n_pca_components = n_pca_components
+            if self.n_pca_components > 0:
+                self.transformer = experiments.transformers.pca_transformer.PCATransformer(n_components=self.n_pca_components)
             self.regressor = LinearRegression()
         
         def fit(self, X, y=None):
-            pca_features = self.transformer.fit_transform(X)
-            self.regressor.fit(pca_features, y)
+            if self.n_pca_components > 0:
+                X = self.transformer.fit_transform(X)
+            self.regressor.fit(X, y)
             return self
     
         def predict(self, X, y=None):
-            pca_features = self.transformer.transform(X)
-            return self.regressor.predict(pca_features)
+            if self.n_pca_components > 0:
+                X = self.transformer.transform(X)
+            return self.regressor.predict(X)
