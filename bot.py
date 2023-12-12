58e1c994f609aff31a656a6d10ee79b453e7edc2
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-12 12:43:29 +0100

    get city name

--- bot.py
+++ bot.py
@@ -17,6 +17,12 @@ MEDIAN_HOUSEHOLDS = os.getenv('MEDIAN_HOUSEHOLDS')
 with open('xgboost.pkl', 'rb') as file:
     xgboost = pickle.load(file)
 
+california_cities_df = pd.read_csv('data/california_cities.csv')
+
+def find_city_by_name(name):
+    name = name.lower().replace(' ', '')
+    return california_cities_df[california_cities_df['Name'].str.lower().replace(' ', '') == name].head(1)
+
 longitude = -122.23
 latitude = 37.88
 housing_median_age = 41.0
@@ -54,20 +60,52 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+step_per_chat = {}
+
+data_per_chat = {}
+
 menu = ReplyKeyboardMarkup([["📈 Predict"]])
-zones = ReplyKeyboardMarkup([["Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5"]])
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!", reply_markup=menu)
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm a bot that predict prices of houses in California", reply_markup=menu)
+    
+    step_per_chat[update.effective_chat.id] = 0
+    data_per_chat[update.effective_chat.id] = {}
 
 async def predict(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Select the zone", reply_markup=zones)
+
+    if step_per_chat[update.effective_chat.id] != 0:
+        return
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Write the name of the city")
+
+    step_per_chat[update.effective_chat.id] = 1
+
+async def get_city_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if step_per_chat[update.effective_chat.id] != 1:
+        return
+
+    city_name = update.message.text
+
+    city = find_city_by_name(city_name)
+
+    if city.empty:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="City not found, try again")
+    else:
+        step_per_chat[update.effective_chat.id] = 2
+        data_per_chat[update.effective_chat.id]['latitude'] = city['Latitude'].values[0]
+        data_per_chat[update.effective_chat.id]['longitude'] = city['Longitude'].values[0]
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="City found with coordinates: " + str(city['Longitude'].values[0]) + ", " + str(city['Latitude'].values[0]))
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
-    application.add_handler(MessageHandler(filters.Regex('Predict'), predict))
+    application.add_handler(MessageHandler(filters.Regex('📈 Predict'), predict))
+    application.add_handler(MessageHandler(filters.Regex('(.*)'), get_city_name))
     
     application.run_polling()
