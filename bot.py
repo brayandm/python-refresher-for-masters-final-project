fb02f14540c0d537a11297004d9714060b131eed
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-12 12:12:13 +0100

    add some buttons

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 from dotenv import load_dotenv
 import pickle
 import pandas as pd
@@ -54,13 +54,20 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+menu = ReplyKeyboardMarkup([["📈 Predict"]])
+zones = ReplyKeyboardMarkup([["Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5"]])
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!", reply_markup=menu)
+
+async def predict(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Select the zone", reply_markup=zones)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    application.add_handler(MessageHandler(filters.Regex('Predict'), predict))
     
     application.run_polling()
