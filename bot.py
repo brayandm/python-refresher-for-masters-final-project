6776c594410fabbbfe59fc94b22b5201d0575b65
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-12 14:16:48 +0100

    update

--- bot.py
+++ bot.py
@@ -139,7 +139,7 @@ async def get_ocean_proximity(update: Update, context: ContextTypes.DEFAULT_TYPE
     'ocean_proximity_NEAR OCEAN': data_per_chat[update.effective_chat.id]['ocean_proximity_NEAR OCEAN']
     }, index=[0])
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="The price of the house is: " + str(xgboost.predict(data)[0]))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="The aproximate price of the house is: " + str(xgboost.predict(data)[0]), reply_markup=menu)
 
     step_per_chat[update.effective_chat.id] = 0
     data_per_chat[update.effective_chat.id] = {}
