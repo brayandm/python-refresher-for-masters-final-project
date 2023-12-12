de8b3f6cad466e67e4940e0b8dde84cd6f672a08
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-12 09:26:51 +0100

    adding paremeters to xgboost

--- experiments/regressors/xgboost_regressor.py
+++ experiments/regressors/xgboost_regressor.py
@@ -8,11 +8,19 @@ reload(experiments.transformers.pca_transformer)
 
 class XGBoostRegressor(BaseEstimator, RegressorMixin):
 
-    def __init__(self, n_pca_components=0):
+    def __init__(self, n_pca_components=0, learning_rate=0.3, max_depth=6, n_estimators=100):
         self.n_pca_components = n_pca_components
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.n_estimators = n_estimators
+
         if self.n_pca_components > 0:
             self.transformer = experiments.transformers.pca_transformer.PCATransformer(n_components=self.n_pca_components)
-        self.regressor = XGBRegressor()
+        self.regressor = XGBRegressor(
+            learning_rate=self.learning_rate,
+            max_depth=self.max_depth,
+            n_estimators=self.n_estimators
+        )
 
     def fit(self, X, y=None):
         X = X.to_numpy()
