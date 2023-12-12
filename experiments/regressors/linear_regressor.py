8ea915e59980e453fd52895fb6f1408a53943b14
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-12-12 07:35:08 +0100

    adding train and test dataset

---
+++ experiments/regressors/linear_regressor.py
@@ -0,0 +1,22 @@
+
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.linear_model import LinearRegression
+
+from importlib import reload
+import experiments.transformers.pca_transformer
+reload(experiments.transformers.pca_transformer)
+
+class LinearRegressor(BaseEstimator, RegressorMixin):
+        
+        def __init__(self, n_components=2):
+            self.transformer = experiments.transformers.pca_transformer.PCATransformer(n_components=n_components)
+            self.regressor = LinearRegression()
+        
+        def fit(self, X, y=None):
+            pca_features = self.transformer.fit_transform(X)
+            self.regressor.fit(pca_features, y)
+            return self
+    
+        def predict(self, X, y=None):
+            pca_features = self.transformer.transform(X)
+            return self.regressor.predict(pca_features)
